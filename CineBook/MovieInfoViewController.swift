96a684e43f4b3aba4e5e4f18300c1379590f4308
Edward <edwvrdh@gmail.com>
2022-05-15 22:11:33 +1000

    Added MovieInfoViewController

---
+++ CineBook/MovieInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieInfoViewController.swift
+//  CineBook
+//
+//  Created by Edward Huang on 15/5/22.
+//
+
+import UIKit
+
+class MovieInfoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.\
+    }
+}
